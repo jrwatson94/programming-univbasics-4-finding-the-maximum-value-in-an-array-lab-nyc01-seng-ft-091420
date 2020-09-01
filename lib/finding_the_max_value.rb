8d0785cf0c6a8f60522d495e14d2fc79cef0ed02
Reid Watson <j.watsonreid@gmail.com>
2020-09-01 16:57:21 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
+  counter = 0
   
+  while array[counter] do
+    max_value = -1
+    if max_value < array[counter]
+      max_value = array[counter]
+    end
+    counter += 1
+  end
 end
